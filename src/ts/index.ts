780f4795c5acb9e2df829c39a2156ea8ec3e1b86
Neill Robson <neill@neillrobson.com>
2019-03-06 12:57:56 -0500

    Add FPS widget

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,14 @@
+import * as Stats from 'stats.js';
 import fragmentSource from '../glsl/fragment.glsl';
 import vertexSource from '../glsl/vertex.glsl';
 import { setupShaderProgram } from './setup';
 import { makeModelData } from './models';
 import { renderModels } from './render';
 import { resize } from './util';
+
+var stats = new Stats();
+stats.showPanel(1);
+document.body.appendChild(stats.dom);
 
 let canvas = document.createElement("canvas");
 document.body.appendChild(canvas);
@@ -21,9 +26,11 @@ gl.clearColor(0, 0, 0, 0);
 gl.clear(gl.DEPTH_BUFFER_BIT || gl.COLOR_BUFFER_BIT);
 
 function render() {
+    stats.begin();
     resize(canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     renderModels(gl, programContainer, modelData);
+    stats.end();
     window.requestAnimationFrame(render);
 }
 render();
