fd20ffbe5c2809b40544177b667f574f94c192e9
Neill Robson <neill@neillrobson.com>
2019-03-20 10:58:28 -0400

    Demonstrate path-marched sphere

--- src/glsl/fragment.glsl
+++ src/glsl/fragment.glsl
@@ -6,7 +6,34 @@ uniform vec2 u_resolution;
 
 out vec4 color;
 
+/**
+ * Signed distance function for sphere with radius 1 centered at origin
+ */
+float sphere(vec3 pos) {
+    return length(pos) - 1.0;
+}
+
 void main() {
-    vec2 uv = gl_FragCoord.xy / u_resolution;
-    color = vec4(uv, 0.0, 1.0);
+    vec2 uv = (gl_FragCoord.xy / u_resolution) * 2.0 - 1.0;
+
+    vec3 camera = vec3(0.0, 0.0, -3.0);
+    vec3 lookAt = normalize(vec3(uv, 1.0));
+
+    vec3 marchTo;
+    float totalStep = 0.0;
+    int i;
+    float marchComplexity;
+    for (i = 0; i < 32; ++i) {
+        marchTo = camera + lookAt * totalStep;
+        float nextStep = sphere(marchTo);
+        if (nextStep < 0.01) break;
+        totalStep += nextStep;
+    }
+    if (i == 32) {
+        marchComplexity = 1.0;
+    } else {
+        marchComplexity = 1.0 - (float(i) / 32.0);
+    }
+
+    color = vec4(marchComplexity, marchComplexity, marchComplexity, 1.0);
 }
