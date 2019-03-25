32e33c129bc9f33a21cad48fd149a5bbc27b75d4
Neill Robson <neill@neillrobson.com>
2019-03-25 12:29:38 -0400

    Render a cube

--- src/glsl/fragment.glsl
+++ src/glsl/fragment.glsl
@@ -15,7 +15,16 @@ float sphere(vec3 pos) {
     return length(pos) - 1.0;
 }
 
+float sdBox(vec3 p, vec3 b) {
+    vec3 d = abs(p) - b;
+    return length(max(d,0.0))
+        + min(max(d.x,max(d.y,d.z)),0.0);
+}
+
 void main() {
+    // The box
+    vec3 boxDimensions = vec3(0.5, 1.0, 1.5);
+
     // uv is (0, 0) at the center of the screen
     vec2 uv = (2.0 * gl_FragCoord.xy - u_resolution) / u_resolution.y;
 
@@ -29,7 +38,8 @@ void main() {
     float marchComplexity;
     for (i = 0; i < 32; ++i) {
         marchTo = u_eye + lookAt * totalStep;
-        float nextStep = sphere(marchTo);
+        // float nextStep = sphere(marchTo);
+        float nextStep = sdBox(marchTo, boxDimensions);
         if (nextStep < 0.01) break;
         totalStep += nextStep;
     }
